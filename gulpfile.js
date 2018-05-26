64f9e5a68ba75f07164a243ca9d81a8d354bdaf3
Blake Lingenau <blake.lingenau@gmail.com>
2018-05-26 14:13:52 -0400

    gulp to tie it all together

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const spawn = require('cross-spawn');
 const gulp = require('gulp');
+const clean = require('gulp-clean');
 const path = require('path');
+const runSequence = require('run-sequence');
 const webpack = require('webpack');
 const config = require('./webpack.dev.js');
 
@@ -9,7 +11,27 @@ gulp.task('copy_html', () => {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('start', ['copy_html'], () => {
+/**
+ * Build the dotnet bridge project into our dist folder.
+ */
+gulp.task('build_bridge', () => {
+  return new Promise((resolve, reject) => {
+    spawn('dotnet publish', ['./lib/bridge', '-c', 'Release', '-r', 'win-x86', '-o', `${__dirname}/dist/resources/win`], { stdio: 'inherit' })
+      .on('close', exitCode => onProcessClosed(exitCode, resolve, reject))
+      .on('error', reject);
+  });
+});
+
+/**
+ * The dotnet building leaves some unwanted files in lib/bridge so we want to clean those up.
+ */
+gulp.task('cleanup_lib', () => {
+  return gulp.src(['lib/bridge/bin', 'lib/bridge/obj'], { read: false })
+    .pipe(clean());
+});
+
+gulp.task('start', () => {
+  runSequence('copy_html', 'build_bridge', 'cleanup_lib')
   return new Promise((resolve, reject) => {
     return webpack(config).run((err, stats) => {
       spawn(getLocalPathForBinary('electron'), ['dist/app.bundle.js'], { stdio: 'inherit' })
